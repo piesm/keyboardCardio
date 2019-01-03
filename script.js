ed4a76664df04df0424bf0010df4fc188be104f8
piesm <mt3616777@gmail.com>
2019-01-03 19:41:03 +0100

    koniec

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const buttonsKey = document.getElementsByClassName('key');
 let lastKey;
 let currentKey;
 let score;
-let endScore = 2;
+let endScore = 20;
 let OffGame = false;
 
 function getRandomNumber(min, max) {
@@ -24,33 +24,43 @@ function getRandomKey() {
 function peep() {
     const time = getRandomNumber(1000, 2000);
     const key = getRandomKey();
-    key.classList.add('fire-key');
+    addClassToElement('fire-key', key);
     currentKey = key;
     // console.log(currentKey.attributes[0].value)
     setTimeout(() => {
-        key.classList.remove('fire-key');
+       
         if (!OffGame) peep();
     }, time);
 };
 
 function endGame() {
     OffGame = true;
+    window.removeEventListener('keydown', checkKey)
+    menu.innerHTML = `<div class="bober1">Victory! Yours score is: ${score}</div> <button onclic="startGame()"><b>Start</b>`
 };
 
-addClassToElement(clasName, element) {
-    // Homework 
-}
+ function addClassToElement(className, element) {
+    element.classList.add(className);
+    const time = getRandomNumber(1000, 2000);
+    setTimeout(() => {
+        removeClassFromElement(className, element);
+    }, time);
+ }  
 
-removeClassToElement(clasName, element) {
-    // net step
-}
+ function removeClassFromElement(className, element) {
+    element.classList.remove(className);
+ }  
+
 
 
 function showScore() {
     menu = document.querySelector('.game-menu');
     // console.log(menu);
     menu.innerHTML = `<div class="scoreBoard">Score: ${score}</div>`; 
-}
+    // if (score = 100){
+    //     menu.innerHTML = `<div class="scoreBoard">Score: Warp!</div>`;  
+    // };
+};
 
 function startGame() {
     score = 0;
@@ -58,18 +68,19 @@ function startGame() {
     window.addEventListener('keydown', checkKey)
     peep();
     OffGame = false;
-    scoreBoard = document.querySelector('.scoreBoard')
+    scoreBoard = document.querySelector('.scoreBoard');
 };
 
 function checkKey(event) {
     if (event.keyCode == currentKey.attributes[0].value) {
         score ++;
-        addClassToElement('.correct-key-down', currentKey);
+        removeClassFromElement('fire-key', currentKey);
+        addClassToElement('correct-key-down', currentKey);
         console.log(score);
     } else {
         score --;
-        let wrongKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
-        addClassToElement('.wrong-key-down', wrongKey);
+        let wrongKey = document.querySelector(`div[data-key="${event.keyCode}"]`);
+        addClassToElement('wrong-key-down', wrongKey);
         console.log(score);
     }
     scoreBoard.innerHTML = `Score: ${score}`;
